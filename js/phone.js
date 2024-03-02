6bae929ae7c3b305a5a35e82759528970ab8f137
Idba1 <moniraislamself@gmail.com>
2024-03-02 12:17:50 +0600

    show all card

--- js/phone.js
+++ js/phone.js
@@ -1,17 +1,41 @@
 // console.log("hello");
-const loadPhone = async () =>{
+const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
-    displayPhones (phones);
+    displayPhones(phones);
 }
 
 
 const displayPhones = phones => {
     // console.log(phones);
+
+    // 1:get container
+    const phoneContainer = document.getElementById('phone-container');
+
     phones.forEach(phones => {
-        console.log(phones)
+        console.log(phones);
+        // 2: create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = "card w-96 bg-gray-100 shadow-xl";
+        // 3: set inner html
+        phoneCard.innerHTML = `
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+
+        <div class="card-body">
+
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+
+        </div>
+        `
+        // append child
+        phoneContainer.appendChild(phoneCard);
     });
 }
 
