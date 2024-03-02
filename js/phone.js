0307c917ae572b7a6b40d3f970347606fb8a5a0c
Idba1 <moniraislamself@gmail.com>
2024-03-02 16:47:28 +0600

    phone search

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,6 @@
 // console.log("hello");
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -13,12 +13,14 @@ const displayPhones = phones => {
 
     // 1:get container
     const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = "";
 
     phones.forEach(phone => {
         console.log(phone);
         // 2: create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = "card w-96 bg-gray-100 shadow-xl";
+        phoneCard.classList = "card p-4 bg-gray-100 shadow-xl";
         // 3: set inner html
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" /></figure>
@@ -34,9 +36,18 @@ const displayPhones = phones => {
 
         </div>
         `
-        // append child
+        // 4: append child
         phoneContainer.appendChild(phoneCard);
     });
 }
 
-loadPhone()
+// handle search button
+const handleSearch = () => {
+    // console.log('search');
+    const searchField = document.getElementById('search-fild');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText)
+}
+
+// loadPhone()
